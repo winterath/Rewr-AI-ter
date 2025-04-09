05419f1c43d1c10d6ab38f77371761324678a60e
winterath <155773922+winterath@users.noreply.github.com>
2025-04-09 19:05:04 +0000

    first commit

---
+++ embed_and_store.py
@@ -0,0 +1,22 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
+
+load_dotenv()
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+
+vector_store = Chroma(
+    collection_name = "knotebooklm",
+    embedding_function=embeddings,
+    persist_directory = "./chroma_langchain_db",
+)
+
+def add_documents(documents):
+    return vector_store.add_documents(documents=documents)
